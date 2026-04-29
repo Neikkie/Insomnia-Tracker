b56f851437cc577b26eb8008beea061c229ff667
Neikkie <neikkie21@outlook.com>
2026-04-29 00:38:49 -0400

    Initial Commit

---
+++ Insomnia Tracker/Insomnia_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insomnia_TrackerApp.swift
+//  Insomnia Tracker
+//
+//  Created by Shanique Beckford on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Insomnia_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
